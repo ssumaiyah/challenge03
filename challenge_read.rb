47aecb94337f111df71aea8ee1721c2abcf8529b
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-06-02 17:21:43 -0500

    deone

--- challenge_read.rb
+++ challenge_read.rb
@@ -2,4 +2,4 @@ require_relative 'ar.rb'
 
 number_of_products = Product.count
 
-puts "There are #{number_of_products} in the products table."jbjbjb
+puts "There are #{number_of_products} in the products table."
